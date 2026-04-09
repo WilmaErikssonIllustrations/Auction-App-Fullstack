0e249a558ba9177273c8d4b92c61067da0789de4
Emma Riklund <emmariklund@hotmail.com>
2026-04-09 11:29:49 +0200

    created auctionContainer

--- frontend/src/productPage.ts
+++ frontend/src/productPage.ts
@@ -1,4 +1,5 @@
 // import type { Auction } from "./models/types";
 // import { hasAuctionEnded } from "./utils/hasAuctionEnded";
 
+const auctionContainer = document.getElementById("auctionContainer");
 
