3f350a4e59b2465cf9729360797ab2ac7fd00884
WilmaErikssonIllustrations <wilmaeriksson1@hotmail.com>
2026-04-17 11:47:19 +0200

    refactor css

--- frontend/src/login.ts
+++ frontend/src/login.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import "./style.css";
+import "./style/style.css";
 
 document.getElementById("loginForm")?.addEventListener("submit", async (e) => {
   e.preventDefault();
