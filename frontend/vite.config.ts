a9f5272c82b414161716152ee4a0e5139c6677e3
WilmaErikssonIllustrations <wilmaeriksson1@hotmail.com>
2026-05-20 14:31:17 +0200

    fix: update navigation, setup vite build config and add root gitignore

---
+++ frontend/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+    import { resolve } from "path";
+
+    export default defineConfig({
+      root: "./", 
+      build: {
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, "index.html"),
+            login: resolve(__dirname, "login.html"),
+            register: resolve(__dirname, "register.html"),
+          },
+        },
+      },
+    });
